260abc82a9f08e3aba59273ee5fa58c32bb793cf
Bastian Henneberg <bh@brandkollektiv.de>
2023-11-08 00:25:12 +0100

    OneDark

--- nvim/init.lua
+++ nvim/init.lua
@@ -171,7 +171,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'dracula-nvim',
+        theme = 'onedark',
         component_separators = '|',
         section_separators = '',
       },
