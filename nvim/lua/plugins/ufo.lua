9e94570aaf24031b457da219b721ad9807aaec23
Bastian Henneberg <bastian.henneberg@gmail.com>
2025-01-13 17:51:50 +0100

    Updates

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -6,13 +6,22 @@ return {
     vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
     vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
+    local ftMap = {
+      vim = 'indent',
+      python = { 'indent' },
+      git = '',
+      markdown = { 'treesitter', 'indent' },
+      php = { 'indent', 'treesitter' },
+      lua = { 'indent', 'treesitter' },
+      vimwiki = { 'treesitter', 'indent' },
+    }
     -- Option 3: treesitter as a main provider instead
     -- (Note: the `nvim-treesitter` plugin is *not* needed.)
     -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
     -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
     require('ufo').setup {
       provider_selector = function(bufnr, filetype, buftype)
-        return { 'indent', 'treesitter' }
+        return ftMap[filetype]
       end,
     }
     --
